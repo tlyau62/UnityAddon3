83a5191ab7498108082e5bf6fa5ca3c34abd0e4d
Tsz Lam Yau <yaurfu@gmail.com>
2019-10-05 09:58:58 +0800

    Add project files.

---
+++ UnityAddon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnityAddon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
