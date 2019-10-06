1d8bfd55972c85fa4abb686dd2a991632e6621a6
Tsz Lam Yau <yaurfu@gmail.com>
2019-10-06 19:03:36 +0800

    Test on transient

---
+++ UnityAddonTest/Bean/TransientBean/TransientBeanTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unity;
+using UnityAddon;
+using UnityAddon.Attributes;
+using Xunit;
+
+namespace UnityAddonTest.Bean.TransientBean
+{
+    [Component]
+    [Scope(ScopeType.Transient)]
+    public class Helper
+    {
+    }
+
+    [Component]
+    public class Service
+    {
+        [Dependency]
+        public Helper Helper { get; set; }
+
+        [Dependency]
+        public Helper Helper2 { get; set; }
+    }
+
+    [Trait("Bean", "TransientBean")]
+    public class TransientBeanTests
+    {
+        [Fact]
+        public void BuildStrategy_DependencyOnTransientBean_BeanInjected()
+        {
+            var container = new UnityContainer();
+            var appContext = new ApplicationContext(container, GetType().Namespace);
+
+            var service = container.Resolve<Service>();
+
+            Assert.NotSame(service.Helper, service.Helper2);
+        }
+    }
+}
