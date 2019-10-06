f83132b6f1e9fa741bd93d51831886c36dba2507
Tsz Lam Yau <yaurfu@gmail.com>
2019-10-06 17:08:04 +0800

    Add test

---
+++ UnityAddonTest/Bean/NonGenericBean/NonGenericBeanTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unity;
+using UnityAddon;
+using UnityAddon.Attributes;
+using Xunit;
+
+namespace UnityAddonTest.Dependency.Bean.NonGenericBean
+{
+    public interface IB { IC c { get; set; } }
+    public interface IC { }
+
+    public abstract class AbstractA
+    {
+        [Dependency]
+        public IB b { get; set; }
+    }
+
+    [Component]
+    public class A : AbstractA
+    {
+    }
+
+    [Component]
+    public class B : IB
+    {
+        [Dependency]
+        public IC c { get; set; }
+    }
+
+    [Component]
+    public class C : IC
+    {
+    }
+
+    [Trait("Bean", "NonGenericBean")]
+    public class NonGenericBeanTests
+    {
+        [Fact]
+        public void BuildStrategy_DependencyOnBean_BeanInjected()
+        {
+            var container = new UnityContainer();
+            var appContext = new ApplicationContext(container, GetType().Namespace);
+
+            Assert.Same(container.Resolve<IB>(), container.Resolve<A>().b);
+            Assert.Same(container.Resolve<IC>(), container.Resolve<IB>().c);
+        }
+    }
+}
