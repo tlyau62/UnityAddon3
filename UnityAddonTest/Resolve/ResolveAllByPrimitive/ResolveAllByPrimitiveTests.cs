37a7c980e03aa29ed06fe3c562de2074f31e72da
Tsz Lam Yau <yaurfu@gmail.com>
2019-10-11 16:12:36 +0800

    Refr resolve all test

---
+++ UnityAddonTest/Resolve/ResolveAllByPrimitive/ResolveAllByPrimitiveTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unity;
+using UnityAddon;
+using UnityAddon.Attributes;
+using Xunit;
+
+namespace UnityAddonTest.Resolve.ResolveAllByPrimitive
+{
+    [Configuration]
+    public class AppConfig
+    {
+        [Bean]
+        [Qualifier("a")]
+        public virtual string CreateStrA()
+        {
+            return "A";
+        }
+
+        [Bean]
+        [Qualifier("b")]
+        public virtual string CreateStrB()
+        {
+            return "B";
+        }
+    }
+
+    [Trait("Resolve", "ResolveAllByPrimitive")]
+    public class ResolveAllByPrimitiveTests
+    {
+        [Fact]
+        public void ApplicationContext_ResolveAllByPrimitive_AllQualifiedBeanResolved()
+        {
+            var container = new UnityContainer();
+            var appContext = new ApplicationContext(container, GetType().Namespace);
+
+            var a = appContext.Resolve<string>("a");
+            var b = appContext.Resolve<string>("b");
+
+            var resolveAll = appContext.ResolveAll<string>();
+
+            Assert.Same(a, resolveAll[0]);
+            Assert.Same(b, resolveAll[1]);
+        }
+    }
+}
