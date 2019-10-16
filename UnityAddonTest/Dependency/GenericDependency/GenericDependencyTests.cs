32a0d9579e72b20cc70689438f4a5bf64ef712f6
Tsz Lam Yau <yaurfu@gmail.com>
2019-10-16 12:42:43 +0800

    Test on generic dependency

---
+++ UnityAddonTest/Dependency/GenericDependency/GenericDependencyTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unity;
+using UnityAddon;
+using UnityAddon.Attributes;
+using Xunit;
+
+namespace UnityAddonTest.Dependency.GenericDependency
+{
+    public abstract class Context { }
+
+    public class AppContext : Context { }
+
+    public interface IContextHelper<T> where T : Context { }
+
+    [Component]
+    public class ContextHelper<T> : IContextHelper<T> where T : Context { }
+
+    public interface IContextFactory<T> where T : Context
+    {
+        IContextHelper<T> ContextHelper { get; set; }
+    }
+
+    [Component]
+    public class ContextFactory<T> : IContextFactory<T> where T : Context
+    {
+        [Dependency]
+        public IContextHelper<T> ContextHelper { get; set; }
+    }
+
+    [Trait("Dependency", "GenericDependency")]
+    public class GenericBeanTests
+    {
+        [Fact]
+        public void BuildStrategy_ResolveGenericDependency_BeanResolved()
+        {
+            var container = new UnityContainer();
+            var appContext = new ApplicationContext(container, GetType().Namespace);
+
+            var contextHelper = appContext.Resolve<IContextHelper<AppContext>>();
+            var contextFactory = appContext.Resolve<IContextFactory<AppContext>>();
+
+            Assert.Same(contextHelper, contextFactory.ContextHelper);
+        }
+    }
+}
