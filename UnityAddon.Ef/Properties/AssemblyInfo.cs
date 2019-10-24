b0ce6115e1fd166f756469c9083c2c489de1eee0
Tim Yau <tyautl@nexify.com.hk>
2019-10-24 17:22:17 +0800

    Upgrade to 0.0.0.9

--- UnityAddon.Ef/Properties/AssemblyInfo.cs
+++ UnityAddon.Ef/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 using UnityAddon.Core.Attributes;
 
-[assembly: AssemblyVersion("0.0.0.8")]
+[assembly: AssemblyVersion("0.0.0.9")]
 [assembly: ComponentScan(BaseNamespaces = new[] { "UnityAddon.Ef" })]
