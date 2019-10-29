c009083e4476f80db1b0876b729fd188f4b80d7e
Tim Yau <tyautl@nexify.com.hk>
2019-10-29 11:55:41 +0800

    Upgrade to .11

--- UnityAddon.Core/Properties/AssemblyInfo.cs
+++ UnityAddon.Core/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 using UnityAddon.Core.Attributes;
 
-[assembly: AssemblyVersion("0.0.0.11")]
+[assembly: AssemblyVersion("0.0.0.12")]
 [assembly: ComponentScan(BaseNamespaces = new[] { "UnityAddon.Core" })]
