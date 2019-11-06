323554b4efc5383b2c61629e34cf6aff320dc39c
Tim Yau <tyautl@nexify.com.hk>
2019-11-06 12:16:02 +0800

    Event queue

---
+++ UnityAddon.Utilities/EventQueue/Event.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnityAddon.Utilities.EventQueue
+{
+    public class Event<T>
+    {
+        public T Data { get; set; }
+        public EventSource EventSource { get; set; }
+    }
+}
