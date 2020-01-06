5c850dbba9815be340bf973a0dc5ea7b90de3dd4
Tim Yau <tyautl@nexify.com.hk>
2020-01-06 17:33:54 +0800

    Update db interceptors

--- UnityAddon.Ef/Transaction/RepositoryInterceptor.cs
+++ UnityAddon.Ef/Transaction/RepositoryInterceptor.cs
@@ -2,12 +2,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Unity;
+using UnityAddon.Core;
 using UnityAddon.Core.Aop;
 using UnityAddon.Core.Attributes;
+using UnityAddon.Core.Reflection;
 
 namespace UnityAddon.Ef.Transaction
 {
@@ -17,12 +21,35 @@ namespace UnityAddon.Ef.Transaction
     [Component]
     public class RepositoryInterceptor : IAttributeInterceptor<RepositoryAttribute>
     {
+        private static readonly MethodInfo DoInDbContextInvokerMethod = typeof(RepositoryInterceptor).GetMethod(nameof(DoInDbContextInvoker), BindingFlags.NonPublic | BindingFlags.Instance);
+
         [Dependency]
-        public IRequireDbContextHandler RequireDbContextHandler { get; set; }
+        public IContainerRegistry ContainerRegistry { get; set; }
+
+        [Dependency]
+        public DataSourceExtractor DataSourceExtractor { get; set; }
 
         public void Intercept(IInvocation invocation)
         {
-            RequireDbContextHandler.DoInDbContext(invocation, false);
+            var dataSource = DataSourceExtractor.ExtractDataSource(invocation.TargetType);
+
+            try
+            {
+                DoInDbContextInvokerMethod
+                    .MakeGenericMethod(dataSource)
+                    .Invoke(this, new object[] { invocation });
+            }
+            catch (TargetInvocationException ex)
+            {
+                ExceptionDispatchInfo.Capture(ex.InnerException).Throw();
+            }
+        }
+
+        private void DoInDbContextInvoker<T>(IInvocation invocation)
+        {
+            var requireDbContextHandler = ContainerRegistry.Resolve<IRequireDbContextHandler<T>>();
+
+            requireDbContextHandler.DoInDbContext(invocation, false);
         }
     }
 }
--- UnityAddon.Ef/Transaction/RequireDbContextInterceptor.cs
+++ UnityAddon.Ef/Transaction/RequireDbContextInterceptor.cs
@@ -3,8 +3,11 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.ExceptionServices;
 using System.Web;
 using Unity;
+using UnityAddon.Core;
 using UnityAddon.Core.Aop;
 using UnityAddon.Core.Attributes;
 using UnityAddon.Core.Reflection;
@@ -17,14 +20,35 @@ namespace UnityAddon.Ef.Transaction
     [Component]
     public class RequireDbContextInterceptor : IAttributeInterceptor<RequireDbContextAttribute>
     {
+        private static readonly MethodInfo DoInDbContextInvokerMethod = typeof(RequireDbContextInterceptor).GetMethod(nameof(DoInDbContextInvoker), BindingFlags.NonPublic | BindingFlags.Instance);
+
         [Dependency]
-        public IRequireDbContextHandler RequireDbContextHandler { get; set; }
+        public IContainerRegistry ContainerRegistry { get; set; }
+
+        [Dependency]
+        public DataSourceExtractor DataSourceExtractor { get; set; }
 
         public void Intercept(IInvocation invocation)
         {
-            var tx = invocation.MethodInvocationTarget.GetAttribute<RequireDbContextAttribute>().Transactional;
+            var dataSource = DataSourceExtractor.ExtractDataSource(invocation.MethodInvocationTarget);
 
-            RequireDbContextHandler.DoInDbContext(invocation, tx);
+            try
+            {
+                DoInDbContextInvokerMethod
+                    .MakeGenericMethod(dataSource)
+                    .Invoke(this, new object[] { invocation, invocation.MethodInvocationTarget.GetAttribute<RequireDbContextAttribute>().Transactional });
+            }
+            catch (Exception ex)
+            {
+                ExceptionDispatchInfo.Capture(ex.InnerException).Throw();
+            }
+        }
+
+        private void DoInDbContextInvoker<T>(IInvocation invocation, bool transactional)
+        {
+            var requireDbContextHandler = ContainerRegistry.Resolve<IRequireDbContextHandler<T>>();
+
+            requireDbContextHandler.DoInDbContext(invocation, transactional);
         }
     }
 }
