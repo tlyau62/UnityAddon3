901aac3040ffb1d39ae8706e51b8225817a4eb0f
Tim Yau <tyautl@nexify.com.hk>
2020-01-06 10:04:08 +0800

    First

---
+++ UnityAddon.Ef/Pagination/Page.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnityAddon.Ef.Pagination
+{
+    public class Page<T>
+    {
+        public IEnumerable<T> Content { get; set; }
+
+        public Pageable Pagination { get; set; }
+    }
+}
---
+++ UnityAddon.Ef/Pagination/PageRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnityAddon.Ef.Pagination
+{
+    public class PageRequest
+    {
+        public static Pageable Of(int page, int size, Sort sort)
+        {
+            return new Pageable(page, size, sort);
+        }
+    }
+}
