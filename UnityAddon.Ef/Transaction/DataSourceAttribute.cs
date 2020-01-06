716cca302de1963ba0c6b0268614b086ccf161d8
Tim Yau <tyautl@nexify.com.hk>
2020-01-06 17:31:17 +0800

    Add datasource attribute

---
+++ UnityAddon.Ef/Transaction/DataSourceAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnityAddon.Ef.Transaction
+{
+    /// <summary>
+    /// Specify the global data source used in db context.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
+    public class DataSourceAttribute : Attribute
+    {
+        public Type Entity { get; set; }
+
+        public DataSourceAttribute(Type entity)
+        {
+            Entity = entity;
+        }
+    }
+}
