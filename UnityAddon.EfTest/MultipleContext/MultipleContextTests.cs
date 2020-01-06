1f3555031a488ac742a23246574f9cc2a9e005c5
Tim Yau <tyautl@nexify.com.hk>
2020-01-06 18:22:21 +0800

    Test insertion on multiple db context

---
+++ UnityAddon.EfTest/MultipleContext/MultipleContextTests.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Unity;
+using UnityAddon.Core;
+using UnityAddon.Ef;
+using UnityAddon.EfTest.Common;
+using Xunit;
+
+namespace UnityAddon.EfTest.MultipleContext
+{
+    [Trait("Transaction", "Repository")]
+    public class MultipleContextTests : IDisposable
+    {
+        private ApplicationContext _appContext;
+        private IDbContextFactory<TestDbContext> _dbContextFactory;
+        private IDbContextFactory<TestDbContext2> _dbContextFactory2;
+        private IRepo _repo;
+
+        public MultipleContextTests()
+        {
+            _appContext = new ApplicationContext(new UnityContainer(), GetType().Namespace, typeof(TestDbContext).Namespace);
+            _dbContextFactory = _appContext.Resolve<IDbContextFactory<TestDbContext>>();
+            _dbContextFactory2 = _appContext.Resolve<IDbContextFactory<TestDbContext2>>();
+            _repo = _appContext.Resolve<IRepo>();
+
+            CreateDb(_dbContextFactory);
+            CreateDb(_dbContextFactory2);
+        }
+
+        public void Dispose()
+        {
+            DropDb(_dbContextFactory);
+            DropDb(_dbContextFactory2);
+        }
+
+        [Fact]
+        public void RepositoryInterceptor_InsertItemsOnMultipleDbContext_ItemsInserted()
+        {
+            var item = new Item("testitem");
+            var item2 = new Item2("testitem2");
+
+            _repo.InsertItem(item);
+            _repo.InsertItem2(item2);
+
+            Assert.Equal(1, _repo.CountItem());
+            Assert.Equal(1, _repo.CountItem2());
+
+            Assert.False(_dbContextFactory.IsOpen());
+            Assert.False(_dbContextFactory2.IsOpen());
+        }
+
+        private void CreateDb<T>(IDbContextFactory<T> dbContextFactory) where T : DbContext
+        {
+            if (!dbContextFactory.IsOpen())
+            {
+                dbContextFactory.Open();
+            }
+
+            dbContextFactory.Get().Database.EnsureCreated();
+            dbContextFactory.Close();
+        }
+
+        private void DropDb<T>(IDbContextFactory<T> dbContextFactory) where T : DbContext
+        {
+            if (!dbContextFactory.IsOpen())
+            {
+                dbContextFactory.Open();
+            }
+
+            dbContextFactory.Get().Database.EnsureDeleted();
+            dbContextFactory.Close();
+        }
+
+    }
+}
