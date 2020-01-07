88c235beea70744fa5ae94e2c33700c2eaccbed0
Tim Yau <tyautl@nexify.com.hk>
2020-01-07 11:30:33 +0800

    upgrade to 13

--- UnityAddon.Ef/Properties/AssemblyInfo.cs
+++ UnityAddon.Ef/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 using UnityAddon.Core.Attributes;
 
-[assembly: AssemblyVersion("0.0.0.12")]
+[assembly: AssemblyVersion("0.0.0.13")]
 [assembly: ComponentScan(BaseNamespaces = new[] { "UnityAddon.Ef" })]
