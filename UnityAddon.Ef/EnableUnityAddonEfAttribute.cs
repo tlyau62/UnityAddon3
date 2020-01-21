e35686f038518fb8e0cb8bf8196bb2c7626b9155
Tim Yau <tyautl@nexify.com.hk>
2020-01-21 10:09:54 +0800

    Fix asm not load

--- UnityAddon.Ef/EnableUnityAddonEfAttribute.cs
+++ UnityAddon.Ef/EnableUnityAddonEfAttribute.cs
@@ -6,8 +6,9 @@ namespace UnityAddon.Ef
 {
     /// <summary>
     /// This attribute ensures the UnityAddonEf assembly is loaded up at the client application.
+    /// Should put attribute at class level. Method level won't be effective.
     /// </summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
     public class EnableUnityAddonEfAttribute : Attribute
     {
     }
