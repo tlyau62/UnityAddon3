636c8202955a9ae47edca744c96f94852023fe0f
Tim Yau <tyautl@nexify.com.hk>
2020-01-21 10:01:24 +0800

    Add enable ef attr

---
+++ UnityAddon.Ef/EnableUnityAddonEfAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnityAddon.Ef
+{
+    /// <summary>
+    /// This attribute ensures the UnityAddonEf assembly is loaded up at the client application.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
+    public class EnableUnityAddonEfAttribute : Attribute
+    {
+    }
+}
