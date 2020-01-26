8887aac8180dddbf45eb98c188110f64755bedbe
Tsz Lam Yau <yaurfu@gmail.com>
2020-01-26 18:14:43 +0800

    Test on no such bean def

---
+++ UnityAddon.CoreTest/DependencyExceptions/NestedNoSuchBeanDefinitionException/NestedNoSuchBeanDefinitionExceptionTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+using UnityAddon.Core;
+using UnityAddon.Core.Attributes;
+using UnityAddon.Core.Exceptions;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace UnityAddon.CoreTest.DependencyExceptions.NestedNoSuchBeanDefinitionException.NestedNoSuchBeanDefinitionExceptionTests
+{
+    public interface IRoot { }
+
+    public interface ICommonService { }
+
+    public interface IService { }
+
+    [Component]
+    public class CommonService : ICommonService
+    {
+        [Dependency]
+        public IRoot Root { get; set; }
+    }
+
+    [Component]
+    public class Service : IService
+    {
+        [Dependency]
+        public ICommonService CommonService { get; set; }
+    }
+
+    [Trait("DependencyExceptions", "NoSuchBeanDefinition")]
+    public class NestedNoSuchBeanDefinitionExceptionTests
+    {
+        [Fact]
+        public void PropertyFill_NestedNoSuchBeanDefinition_ExceptionThrown()
+        {
+            var container = new UnityContainer();
+            var appContext = new ApplicationContext(container, false, GetType().Namespace);
+
+            var ex = Assert.Throws<NoSuchBeanDefinitionException>(() => appContext.Resolve<IService>());
+
+            Assert.Equal($"Property Root in {typeof(CommonService).FullName} required a bean of type '{typeof(IRoot).FullName}' that could not be found.", ex.Message);
+        }
+    }
+
+}
