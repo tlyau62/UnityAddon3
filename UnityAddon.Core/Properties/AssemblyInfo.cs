e88506a6ea2bc0305c335776f18fbcc2c89ee851
Tsz Lam Yau <yaurfu@gmail.com>
2020-01-29 11:02:29 +0800

    Upgrade to 22

--- UnityAddon.Core/Properties/AssemblyInfo.cs
+++ UnityAddon.Core/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 using UnityAddon.Core.Attributes;
 
-[assembly: AssemblyVersion("0.0.0.21")]
+[assembly: AssemblyVersion("0.0.0.22")]
 [assembly: ComponentScan(BaseNamespaces = new[] { "UnityAddon.Core" })]
