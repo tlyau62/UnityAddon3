c036cdd570e689c390d12bffa618365ed6755e47
Tsz Lam Yau <yaurfu@gmail.com>
2020-02-13 10:01:52 +0800

    Update to 0.0.0.25

--- UnityAddon.Core/Properties/AssemblyInfo.cs
+++ UnityAddon.Core/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 using UnityAddon.Core.Attributes;
 
-[assembly: AssemblyVersion("0.0.0.24")]
+[assembly: AssemblyVersion("0.0.0.25")]
 [assembly: ComponentScan(BaseNamespaces = new[] { "UnityAddon.Core" })]
