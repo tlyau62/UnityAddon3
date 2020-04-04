27091999e41df2c34071eb071620a13e1f7d917f
Tsz Lam Yau <yaurfu@gmail.com>
2020-04-04 16:17:54 +0800

    Add test on null resolve by null test

---
+++ UnityAddon.CoreTest/Resolve/ResolveByNull/ResolveByNullTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unity;
+using UnityAddon.Core;
+using UnityAddon.Core.Attributes;
+using Xunit;
+
+namespace UnityAddon.CoreTest.Resolve.ResolveByNull
+{
+    public interface IService { }
+
+    [Component]
+    [Qualifier("test")]
+    public class Service : IService { }
+
+    [Trait("Resolve", "ResolveByNull")]
+    public class ResolveByNullTests : UnityAddonDefaultTest
+    {
+        [Dependency]
+        public IUnityContainer UnityContainer { get; set; }
+
+        [Dependency]
+        public IService Service { get; set; }
+
+        [Fact]
+        public void BeanDefinitionRegistry_ResolveByNull_BeanResolved()
+        {
+            Assert.IsType<Service>(UnityContainer.ResolveUA<IService>());
+            Assert.IsType<Service>(Service);
+        }
+    }
+}
