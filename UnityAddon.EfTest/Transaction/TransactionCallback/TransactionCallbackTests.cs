57c82ba399844398ef82a1ff0ddcfb7a6069b768
Tsz Lam Yau <yaurfu@gmail.com>
2020-04-06 17:42:20 +0800

    Add tx callback

---
+++ UnityAddon.EfTest/Transaction/TransactionCallback/TransactionCallbackTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unity;
+using UnityAddon.Core.Attributes;
+using UnityAddon.Ef.Transaction;
+using UnityAddon.EfTest.Common;
+using Xunit;
+
+namespace UnityAddon.EfTest.Transaction.TransactionCallback
+{
+    [Component]
+    public class Logger
+    {
+        public string Log = "";
+    }
+
+    public class TransactionCallbackTests : EfDefaultTest<TestDbContext>
+    {
+        [Dependency]
+        public IDbContextTemplate DbContextTemplate { get; set; }
+
+        [Dependency]
+        public Logger Logger { get; set; }
+
+        [Fact]
+        public void TransactionInterceptorManager_ExecuteTransaction_InterceptorExecuted()
+        {
+            DbContextTemplate.ExecuteTransaction<TestDbContext, object>(tx =>
+            {
+                DbContextTemplate.RegisterTransactionCallback(() => Logger.Log += "A");
+
+                Logger.Log += "B";
+
+                DbContextTemplate.ExecuteTransaction<TestDbContext, object>(tx =>
+                {
+                    DbContextTemplate.RegisterTransactionCallback(() => Logger.Log += "C");
+
+                    Logger.Log += "D";
+
+                    DbContextTemplate.ExecuteTransaction<TestDbContext, object>(tx =>
+                    {
+                        DbContextTemplate.RegisterTransactionCallback(() => Logger.Log += "E");
+
+                        Logger.Log += "F";
+
+                        return null;
+                    });
+
+                    return null;
+                });
+
+                return null;
+            });
+
+            Assert.Equal("BDFACE", Logger.Log);
+        }
+    }
+}
