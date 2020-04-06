1f4fd2856989ba5962193b6feb03934324f0640d
Tsz Lam Yau <yaurfu@gmail.com>
2020-04-06 11:03:24 +0800

    Remove repeated logic in db context

--- UnityAddon.Ef/Transaction/DbContextTemplate.cs
+++ UnityAddon.Ef/Transaction/DbContextTemplate.cs
@@ -5,10 +5,11 @@ using UnityAddon.Core.Attributes;
 
 namespace UnityAddon.Ef.Transaction
 {
-    public interface IDbContextTemplate<T> where T : DbContext
+    public interface IDbContextTemplate<TDbContext> where TDbContext : DbContext
     {
-        void ExecuteTransaction(Action<T> transaction);
-        T GetDbContext();
+        TTxResult ExecuteTransaction<TTxResult>(Func<TDbContext, TTxResult> transaction);
+        TTxResult ExecuteQuery<TTxResult>(Func<TDbContext, TTxResult> query, string noModifyMsg);
+        TDbContext GetDbContext();
         DbSet<TEntity> GetEntity<TEntity>() where TEntity : class;
     }
 
@@ -16,43 +17,68 @@ namespace UnityAddon.Ef.Transaction
     /// Used by client to query db.
     /// TODO: logic duplication with TransactionManager
     /// </summary>
-    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="TDbContext"></typeparam>
     [Component]
-    public class DbContextTemplate<T> : IDbContextTemplate<T> where T : DbContext
+    public class DbContextTemplate<TDbContext> : IDbContextTemplate<TDbContext> where TDbContext : DbContext
     {
         [Dependency]
-        public IDbContextFactory<T> DbContextFactory { get; set; }
+        public IDbContextFactory<TDbContext> DbContextFactory { get; set; }
 
-        public void ExecuteTransaction(Action<T> transaction)
+        [OptionalDependency]
+        public RollbackOptions RollbackOptions { get; set; }
+
+        public TTxResult ExecuteQuery<TTxResult>(Func<TDbContext, TTxResult> query, string noModifyMsg)
         {
-            var isOpenDbCtx = DbContextFactory.IsOpen();
-            T ctx = isOpenDbCtx ? DbContextFactory.Get() : DbContextFactory.Open();
-            var tx = ctx.Database.BeginTransaction();
-
-            try
+            return DoInDbContext(ctx =>
             {
-                transaction(ctx);
+                var result = query(ctx);
 
-                ctx.SaveChanges();
-                tx.Commit();
-            }
-            catch (Exception)
-            {
-                tx.Rollback();
-                throw;
-            }
-            finally
-            {
-                tx.Dispose();
+                AssertNoModifyDbContext(ctx, noModifyMsg);
 
-                if (!isOpenDbCtx)
-                {
-                    DbContextFactory.Close();
-                }
-            }
+                return result;
+            });
         }
 
-        public T GetDbContext()
+        public TTxResult ExecuteTransaction<TTxResult>(Func<TDbContext, TTxResult> transaction)
+        {
+            return DoInDbContext(ctx =>
+            {
+                if (ctx.Database.CurrentTransaction != null)
+                {
+                    return transaction(ctx);
+                }
+
+                var tx = ctx.Database.BeginTransaction();
+
+                try
+                {
+                    var result = transaction(ctx);
+
+                    if (RollbackOptions != null && RollbackOptions.TestRollback(result))
+                    {
+                        tx.Rollback();
+                    }
+                    else
+                    {
+                        ctx.SaveChanges();
+                        tx.Commit();
+                    }
+
+                    return result;
+                }
+                catch (Exception)
+                {
+                    tx.Rollback();
+                    throw;
+                }
+                finally
+                {
+                    tx.Dispose();
+                }
+            });
+        }
+
+        public TDbContext GetDbContext()
         {
             return DbContextFactory.Get();
         }
@@ -60,6 +86,32 @@ namespace UnityAddon.Ef.Transaction
         public DbSet<TEntity> GetEntity<TEntity>() where TEntity : class
         {
             return GetDbContext().Set<TEntity>();
+        }
+
+        private void AssertNoModifyDbContext(TDbContext dbContext, string noModifyMsg = "Detected dbcontext is changed by method, but transaction is not opened.")
+        {
+            if (dbContext.ChangeTracker.HasChanges() && dbContext.Database.CurrentTransaction == null)
+            {
+                throw new InvalidOperationException(noModifyMsg);
+            }
+        }
+
+        public TTxResult DoInDbContext<TTxResult>(Func<TDbContext, TTxResult> tx)
+        {
+            var isOpenDbCtx = DbContextFactory.IsOpen();
+            var ctx = isOpenDbCtx ? DbContextFactory.Get() : DbContextFactory.Open();
+
+            try
+            {
+                return tx(ctx);
+            }
+            finally
+            {
+                if (!isOpenDbCtx)
+                {
+                    DbContextFactory.Close();
+                }
+            }
         }
     }
 }
