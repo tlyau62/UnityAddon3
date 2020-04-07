9c078e915a6a51c414e0355d0a1d21be91b600a6
Tsz Lam Yau <yaurfu@gmail.com>
2020-04-07 18:05:02 +0800

    Fix comp scan callback

--- UnityAddon.Hangfire/UnityAddonHangfireHostBuilder.cs
+++ UnityAddon.Hangfire/UnityAddonHangfireHostBuilder.cs
@@ -4,19 +4,20 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
 using UnityAddon.Core;
+using UnityAddon.Core.Component;
 
 namespace UnityAddon.Hangfire
 {
     public static class UnityAddonHangfireHostBuilder
     {
-        public static IHostBuilder ScanComponentsUAHangfire(this IHostBuilder hostBuilder, Assembly assembly, params string[] namespaces)
-        {
-            return hostBuilder.ScanComponentsUA(assembly, b => b.AddComponentScannerStrategy<HangfireComponentScannerStrategy>(), namespaces);
-        }
-
         public static IHostBuilder EnableUnityAddonHangfireEf(this IHostBuilder hostBuilder)
         {
-            return hostBuilder.ScanComponentsUA(Assembly.GetExecutingAssembly(), "UnityAddon.Hangfire");
+            return hostBuilder
+                .ConfigureUA<ComponentScannerBuilder>(c =>
+                {
+                    c.AddComponentScannerStrategy<HangfireComponentScannerStrategy>();
+                })
+                .ScanComponentsUA(Assembly.GetExecutingAssembly(), "UnityAddon.Hangfire");
         }
     }
 }
