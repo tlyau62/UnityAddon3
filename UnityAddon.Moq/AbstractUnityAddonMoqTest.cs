8cf273fb821037a736e20ccc8611356f8f6ac474
Samuel Fung <sfungps@nexify.com.hk>
2020-04-15 12:30:07 +0800

    moq test support

---
+++ UnityAddon.Moq/AbstractUnityAddonMoqTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Unity;
+using UnityAddon.Core;
+
+namespace UnityAddon.Moq
+{
+    public abstract class AbstractUnityAddonMoqTest
+    {
+        public AbstractUnityAddonMoqTest()
+        {
+            var host = new HostBuilder()
+                .RegisterUA()
+                .EnableUnityAddonMoq()
+                .BuildUA();
+
+            var container = host.Services.GetService(typeof(IUnityContainer)) as IUnityContainer;
+            container.BuildUp(this.GetType(), this);
+        }
+    }
+}
---
+++ UnityAddon.Moq/TestSubjectAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnityAddon.Moq
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class TestSubjectAttribute : Attribute
+    {
+    }
+}
