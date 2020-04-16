637f4b2d1c496b857e900399af92d6e5f08cb066
Tim Yau <tyautl@nexify.com.hk>
2020-04-16 17:19:49 +0800

    Add partial inject dep

---
+++ UnityAddon.MoqTest/PartialDependencyTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unity;
+using UnityAddon.Core.Attributes;
+using UnityAddon.Moq;
+using Xunit;
+
+namespace UnityAddon.MoqTest.PartialDependency
+{
+    public interface IMessageRepository
+    {
+        string GetMessage();
+    }
+
+    [Component]
+    public class MessageService
+    {
+        [Dependency]
+        public IMessageRepository MessageRepository { get; set; }
+    }
+
+    public class PartialDependencyTest : AbstractUnityAddonMoqTest
+    {
+        [Dependency]
+        public MessageService MessageService { get; set; }
+
+        public PartialDependencyTest() : base(true) { }
+
+        [Fact]
+        public void Test()
+        {
+            Assert.Null(MessageService.MessageRepository);
+        }
+    }
+}
