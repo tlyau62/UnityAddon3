84eada7b9d3c3e9de3e027b9d3426361b144af6d
Tsz Lam Yau <yaurfu@gmail.com>
2020-04-30 15:40:37 +0800

    Refr test

---
+++ UnityAddon.CoreIntegrationTest/BeanPostConstruct/NonProxyBean/NonProxyBeanTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unity;
+using UnityAddon;
+using UnityAddon.Core;
+using UnityAddon.Core.Attributes;
+using Xunit;
+
+namespace UnityAddon.CoreTest.BeanPostConstruct.NonProxy
+{
+    [Component]
+    public class StringStore
+    {
+        public string TestString { get; set; } = "";
+    }
+
+    [Component]
+    public class ServiceA
+    {
+        [Dependency]
+        public StringStore StringStore { get; set; }
+
+        [PostConstruct]
+        public void Init()
+        {
+            StringStore.TestString += "A";
+        }
+    }
+
+    [Component]
+    public class ServiceB
+    {
+        [Dependency]
+        public StringStore StringStore { get; set; }
+
+        [Dependency]
+        public ServiceA ServiceA { get; set; }
+
+        [PostConstruct]
+        public void Init()
+        {
+            Assert.NotNull(ServiceA);
+            StringStore.TestString += "B";
+        }
+    }
+
+    [Component]
+    public class ServiceC
+    {
+        [Dependency]
+        public StringStore StringStore { get; set; }
+
+        [Dependency]
+        public ServiceB ServiceB { get; set; }
+
+        [PostConstruct]
+        public void Init()
+        {
+            Assert.NotNull(ServiceB);
+            StringStore.TestString += "C";
+        }
+    }
+
+    [Trait("BeanPostConstruct", "NonProxyBean")]
+    public class NonProxyBeanTests : UnityAddonDefaultTest
+    {
+        [Dependency]
+        public IUnityContainer UnityContainer { get; set; }
+
+        [Theory]
+        [InlineData(0, 1, 2)]
+        [InlineData(0, 2, 1)]
+        [InlineData(1, 0, 2)]
+        [InlineData(1, 2, 0)]
+        [InlineData(2, 1, 0)]
+        [InlineData(2, 0, 1)]
+        public void BuildStrategy_PostConstructNonProxyBean_BeanPostConstructed(int orderA, int orderB, int orderC)
+        {
+            var stringStore = UnityContainer.Resolve<StringStore>();
+            var resolveOrder = new Type[3];
+
+            resolveOrder[orderA] = typeof(ServiceA);
+            resolveOrder[orderB] = typeof(ServiceB);
+            resolveOrder[orderC] = typeof(ServiceC);
+
+            foreach (var t in resolveOrder)
+            {
+                UnityContainer.Resolve(t);
+            }
+
+            Assert.Equal("ABC", stringStore.TestString);
+        }
+    }
+}
