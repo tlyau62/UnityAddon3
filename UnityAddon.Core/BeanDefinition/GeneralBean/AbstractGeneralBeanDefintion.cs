aa895aa051a2423ff65c0ebdac27c96b98ec9171
Tim Yau <tyautl@nexify.com.hk>
2020-05-04 11:39:59 +0800

    Add general type bean def

---
+++ UnityAddon.Core/BeanDefinition/GeneralBean/AbstractGeneralBeanDefintion.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unity.Lifetime;
+using UnityAddon.Core.Attributes;
+
+namespace UnityAddon.Core.BeanDefinition.GeneralBean
+{
+    public abstract class AbstractGeneralBeanDefintion : IBeanDefinition
+    {
+        private readonly ScopeType _scopedType;
+
+        private LifetimeManager _scope;
+
+        public AbstractGeneralBeanDefintion(Type type, string name, ScopeType scopeType)
+        {
+            _scopedType = scopeType;
+            Type = type;
+            Qualifiers = new[] { name };
+        }
+
+        public Type Type { get; set; } = typeof(object);
+
+        public abstract string Name { get; }
+
+        public LifetimeManager Scope
+        {
+            get
+            {
+                if (_scope == null)
+                {
+                    if (_scopedType == ScopeType.Scoped)
+                    {
+                        _scope = Activator.CreateInstance<HierarchicalLifetimeManager>();
+                    }
+                    else if (_scopedType == ScopeType.Singleton)
+                    {
+                        _scope = Activator.CreateInstance<ContainerControlledLifetimeManager>();
+                    }
+                    else if (_scopedType == ScopeType.Transient)
+                    {
+                        _scope = Activator.CreateInstance<ContainerControlledTransientManager>();
+                    }
+                    else
+                    {
+                        throw new NotImplementedException();
+                    }
+                }
+
+                return _scope;
+            }
+        }
+
+        public Type[] AutoWiredTypes => new[] { Type };
+
+        public string[] Qualifiers { get; set; } = new string[0];
+
+        public string[] Profiles { get; set; } = new string[0];
+
+        public bool IsPrimary { get; set; } = false;
+
+        public string Namespace => Type.Namespace;
+
+        public bool FromComponentScanning { get; set; } = false;
+
+        public abstract object Constructor(IServiceProvider serviceProvider, Type type, string name);
+    }
+}
---
+++ UnityAddon.Core/ServicePostRegistry.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Unity;
+using UnityAddon.Core.Bean;
+
+namespace UnityAddon.Core
+{
+    public interface IServicePostRegistry
+    {
+        void AddSingleton(Type type, string name);
+        void AddSingleton(object instance, string name);
+        void AddSingleton(Func<IServiceProvider, object> factory, string name);
+    }
+
+    public class ServicePostRegistry : IServicePostRegistry
+    {
+        [Dependency]
+        public ContainerBuilder ContainerBuilder { get; set; }
+
+        public void AddSingleton(Type type, string name)
+        {
+            var entry = new ContainerBuilderEntry(ContainerBuilderEntryOrder.App, false);
+
+            entry.ConfigureBeanDefinitions(config =>
+            {
+
+            });
+
+            throw new NotImplementedException();
+        }
+
+        public void AddSingleton(object instance, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddSingleton(Func<IServiceProvider, object> factory, string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
