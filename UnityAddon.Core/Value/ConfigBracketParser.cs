3ddcf6404e734a70b295ac2e4f4e61984730d52d
Tim Yau <tyautl@nexify.com.hk>
2020-05-06 11:39:08 +0800

    Replace optional dep to dep

--- UnityAddon.Core/Value/ConfigBracketParser.cs
+++ UnityAddon.Core/Value/ConfigBracketParser.cs
@@ -15,18 +15,13 @@ namespace UnityAddon.Core.Value
     [Component]
     public class ConfigBracketParser : AbstrackBracketParser
     {
-        [OptionalDependency]
+        [Dependency]
         public IConfiguration Config { get; set; }
 
         private static readonly Regex DefaultValue = new Regex("^([^:\n]*)(:([^:\n]*))?$", RegexOptions.Compiled);
 
         protected override string Process(string intermediateResult)
         {
-            if (Config == null)
-            {
-                return "not configured";
-            }
-
             if (!DefaultValue.IsMatch(intermediateResult))
             {
                 throw new FormatException();
