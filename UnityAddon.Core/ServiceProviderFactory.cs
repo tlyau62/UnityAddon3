2b945ae84689171efb747ba47ad661e79f9eaf93
Tim Yau <tyautl@nexify.com.hk>
2020-05-06 10:31:37 +0800

    Remove junk

--- UnityAddon.Core/ServiceProviderFactory.cs
+++ UnityAddon.Core/ServiceProviderFactory.cs
@@ -26,11 +26,8 @@ namespace UnityAddon.Core
     {
         private readonly IUnityContainer _container;
 
-        private bool _isNewContainer = false;
-
         public ServiceProviderFactory() : this(new UnityContainer())
         {
-            _isNewContainer = true;
         }
 
         public ServiceProviderFactory(IUnityContainer container)
@@ -51,37 +48,6 @@ namespace UnityAddon.Core
         public IServiceProvider CreateServiceProvider(ApplicationContext appCtx)
         {
             return appCtx.Build();
-
-            //var beanDefFilters = _sp.GetRequiredService<BeanDefintionCandidateSelector>();
-            //var beanDefCollection = compScannedDefs.Where(d => !d.FromComponentScanning || beanDefFilters.Filter(d));
-
-            //_beanDefContainer.RegisterBeanDefinitions(beanDefCollection);
-
-            //_container.AddNewExtension<AopBuildStrategyExtension>();
-
-            //foreach (var beanDef in beanDefCollection)
-            //{
-            //    _container.RegisterFactory(beanDef.Type, beanDef.Name, (c, t, n) => beanDef.Constructor(_sp, t, n), (IFactoryLifetimeManager)beanDef.Scope);
-            //}
-
-            //foreach (var defCollection in _sp.GetServices<IBeanDefinitionCollection>())
-            //{
-            //    _beanDefContainer.RegisterBeanDefinitions(defCollection);
-
-            //    foreach (var beanDef in defCollection)
-            //    {
-            //        _container.RegisterFactory(beanDef.Type, beanDef.Name, (c, t, n) => beanDef.Constructor(_sp, t, n), (IFactoryLifetimeManager)beanDef.Scope);
-            //    }
-            //}
-
-            //var _sp = _container.Resolve<IServiceProvider>();
-
-            //if (_isNewContainer)
-            //{
-            //    _sp.GetService<IHostApplicationLifetime>()?.ApplicationStopped.Register(() => _container.Dispose());
-            //}
-
-            //return _sp;
         }
     }
 }
