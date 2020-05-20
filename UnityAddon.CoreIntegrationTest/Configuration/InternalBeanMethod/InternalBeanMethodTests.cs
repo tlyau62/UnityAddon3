5175541b488256b30b07732593d148733ca68ec5
Tim Yau <tyautl@nexify.com.hk>
2020-05-20 16:13:28 +0800

    Test on internal bean method

---
+++ UnityAddon.CoreIntegrationTest/Configuration/InternalBeanMethod/InternalBeanMethodTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unity;
+using UnityAddon.Core;
+using UnityAddon.Core.Attributes;
+using Xunit;
+
+namespace UnityAddon.CoreIntegrationTest.Configuration.InternalBeanMethod
+{
+    public interface IService { }
+
+    public class Service : IService { }
+
+    [Configuration]
+    public class Config
+    {
+        [Bean]
+        internal virtual IService Service()
+        {
+            return new Service();
+        }
+    }
+
+    [ComponentScan]
+    public class InternalBeanMethodTests : UnityAddonTest
+    {
+        [Dependency]
+        public IService Service { get; set; }
+
+        [Fact]
+        public void BeanMethod()
+        {
+            Assert.NotNull(Service);
+        }
+    }
+}
