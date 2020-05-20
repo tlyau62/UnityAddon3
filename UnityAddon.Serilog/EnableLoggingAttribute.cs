30154246adf5852a2bad8be516bb651195345660
Tim Yau <tyautl@nexify.com.hk>
2020-05-20 14:48:28 +0800

    Add enable logging attribute

---
+++ UnityAddon.Serilog/EnableLoggingAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityAddon.Core;
+using UnityAddon.Core.Attributes;
+using UnityAddon.CoreTest.Bean;
+
+namespace UnityAddon.Serilog
+{
+    public class EnableLoggingAttribute : ConfigArgAttribute
+    {
+        public EnableLoggingAttribute()
+        {
+            var config = typeof(SerilogConfig);
+
+            Args.Add(new object[] { UnityAddonTest.CONFIG_PREFIX + config.Name, config, typeof(Type) });
+        }
+    }
+}
