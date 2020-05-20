776b83e80a8a9d79064441374b4ee02d3fb3f141
Tim Yau <tyautl@nexify.com.hk>
2020-05-20 14:26:28 +0800

    Move logger to individual proj

---
+++ UnityAddon.Serilog/SerilogConfig.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.Text;
+using Unity;
+using UnityAddon.Core.Attributes;
+using UnityAddon.Core.BeanDefinition;
+
+namespace UnityAddon.Serilog
+{
+    public class SerilogConfig
+    {
+        [Bean]
+        public virtual IBeanDefinitionCollection Serilog(HostBuilderContext hostBuilderContext, [OptionalDependency] Action<HostBuilderContext, LoggerConfiguration> configureLogger)
+        {
+            IBeanDefinitionCollection defCol = new BeanDefinitionCollection();
+
+            defCol.AddFromServiceCollection(services =>
+            {
+                new SerilogServiceCollectionTrap(hostBuilderContext, services).UseSerilog((hostContext, loggerConfig) =>
+                {
+                    configureLogger(hostContext, loggerConfig);
+                });
+            });
+
+            return defCol;
+        }
+    }
+}
--- UnityAddon.SerilogTest/SerilogConfigTests.cs
+++ UnityAddon.SerilogTest/SerilogConfigTests.cs
@@ -3,46 +3,44 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Core;
+using Serilog.Sinks.InMemory;
+using Serilog.Sinks.InMemory.Assertions;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using Unity;
 using Unity.Builder;
 using UnityAddon.Core;
 using UnityAddon.Core.Attributes;
-using UnityAddon.Core.Util.Serilog;
+using UnityAddon.Serilog;
 using Xunit;
 
-namespace UnityAddon.CoreTest.Util.Serilog
+namespace UnityAddon.SerilogTest
 {
     public class TestConfig
     {
         [Bean]
         public virtual Action<HostBuilderContext, LoggerConfiguration> LoggerConfig(IConfiguration config)
         {
-            return (hostContext, loggerConfig) => loggerConfig.ReadFrom.Configuration(config);
-        }
-
-        [Bean]
-        public virtual IConfiguration Config()
-        {
-            var config = new ConfigurationBuilder().AddJsonFile(@"C:\Users\tyautl\Github\UnityAddon3\UnityAddon.CoreTest\Util\Serilog\appsettings.json")
-                .Build();
-
-            return config;
+            return (hostContext, loggerConfig) => loggerConfig.WriteTo.InMemory();
         }
     }
 
     [ContextConfiguration(typeof(TestConfig), typeof(SerilogConfig))]
     public class SerilogConfigTests : UnityAddonTest
     {
-        //[Dependency]
-        //public ILogger<SerilogConfigTests> Logger { get; set; }
-
         [Fact]
         public void ConfigTest()
         {
-            Log.Debug("You should click the clap button if you found this post useful2!");
+            var randomLog = Guid.NewGuid().ToString();
+
+            Log.Information(randomLog);
+
+            InMemorySink.Instance.Should()
+                .HaveMessage(randomLog)
+                .Appearing()
+                .Once();
         }
     }
 }
