347b52bdaa12958fffcde645ca7fa98ac7399360
Tim Yau <tyautl@nexify.com.hk>
2020-05-22 14:30:28 +0800

    Doc import attr

--- UnityAddon.Core/Attributes/ImportAttribute.cs
+++ UnityAddon.Core/Attributes/ImportAttribute.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace UnityAddon.Core.Attributes
 {
+    /// <summary>
+    /// Indicates one or more component classes to import — typically @Configuration classes.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
     public class ImportAttribute : Attribute
     {
