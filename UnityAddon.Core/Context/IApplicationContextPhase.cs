a95148cac26181a1a19370bb0364e0f72160bebf
Tim Yau <tyautl@nexify.com.hk>
2020-05-28 11:37:44 +0800

    Add appctx phase interface

---
+++ UnityAddon.Core/Context/IApplicationContextPhase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnityAddon.Core.Context
+{
+    /// <summary>
+    /// Process is invoked before the current phase is started.
+    /// </summary>
+    public interface IApplicationContextPhase
+    {
+        void Process();
+    }
+
+    public interface IAppCtxPostServiceRegistrationPhase : IApplicationContextPhase
+    {
+    }
+
+    public interface IAppCtxPreInstantiateSingletonPhase : IApplicationContextPhase
+    {
+    }
+
+    public interface IAppCtxFinishPhase : IApplicationContextPhase
+    {
+    }
+}
